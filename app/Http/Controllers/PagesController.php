a20a3cba350dc2fe3c12a2aaaef2297252bbc9ae
CarlosPxndx94 <carlossolorock@gmail.com>
2016-03-24 22:29:56 -0500

    get orderBy take(limitar)

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,12 +4,17 @@ namespace EloquentBasico\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use EloquentBasico\User;
+
 use EloquentBasico\Http\Requests;
 use EloquentBasico\Http\Controllers\Controller;
 
 class PagesController extends Controller
 {
     public function home(){
-    	return view('pages.home');
+    	$users = User::orderBy('id', 'DESC')
+    			->take(10)
+    			->get();
+    	return view('pages.home', compact('users'));
     }
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -11,5 +11,22 @@
 	        </div>  
 	    </div>
 
+		<div class="container">
+			<h1 class="page-header">Ultimos Usuarios Registrados</h1>
+			<table class="table table-hover table-striped">
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Nombre</th>
+						<th>E-mail</th>
+						<th>Genero</th>
+						<th>Biografia</th>
+					</tr>
+				</thead>
+				<tbody>
+					 @include('partials.list-users')
+				</tbody>
+			</table>
+		</div>
 	@stop
 </div>		
